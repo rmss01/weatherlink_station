4b855584496b9e7311ea5925ec70f4b23356bf4d
rmss <ramurai2001@gmail.com>
2024-06-26 13:20:55 -0600

    stationHistoric.js + history.html

--- js/stationHistoric.js
+++ js/stationHistoric.js
@@ -1,18 +1,102 @@
-// Prueba de obtencion de data histórica
-// NO FUNCIONA
-async function historica(station, startTimestamp, endTimestamp){
-    const archivo = `http://localhost:3003/stations/historic/${station}&${startTimestamp}&${endTimestamp}`;
-    const resultado = await fetch(archivo);
+let dataTable;
+let dataTableIsInisialized = false;
 
-    let historic = await resultado.json();
-
-    return historic;
+const dataTableOptions = {
+    dom: "Bfrtip",
+    buttons: {
+        dom:{
+            button:{
+                className: 'btn'
+            }
+        },
+        buttons:[
+            {
+                extend: "excel",
+                text: "Exportar Excel",
+                className: "btn btn-outline-success",
+                excelStyles: {                // Add an excelStyles definition
+                    template: "header_blue",  // Apply the 'blue_medium' template
+                }
+            }
+        ]},
+    lengthMenu: [10, 20, 50, 100, 200],
+    destroy: true,
+    destroy: true,
+    language: {
+        "decimal": "",
+        "emptyTable": "No hay datos disponibles en la tabla",
+        "info": "Mostrando de _START_ a _END_ de un total de _TOTAL_ registros",
+        "infoEmpty": "Mostrando 0 a 0 de 0 registros",
+        "infoFiltered": "(filtrados desde _MAX_ registros totales)",
+        "infoPostFix": "",
+        "thousands": ",",
+        "lengthMenu": "Mostrar _MENU_ registros por página",
+        "loadingRecords": "Cargando ...",
+        "processing": "",
+        "search": "Buscar:",
+        "zeroRecords": "No se encontró ningún registro",
+        "paginate": {
+            "first": "Primero",
+            "last": "Último",
+            "next": "Siguiente",
+            "previous": "Anterior"
+        },
+        "aria": {
+            "orderable": "Ordenar por ésta columna",
+            "orderableReverse": "Invertir el órden por esta columna"
+        }
+    }
 }
 
-$(document).ready(async function () {
-    
-    var end = Math.floor(Date.now() / 1000);
-    var start = end - 86400;
-    const historic = await historica(178073, start, end);
-    console.log(historic);
-});
+const initDataTable = async () => {
+    if (dataTableIsInisialized) {
+        dataTable.destroy();
+    }
+
+    await loadData();
+
+    dataTable = $("#datatable").DataTable(dataTableOptions);
+    dataTableIsInisialized = true;
+}
+const loadData = async () => {
+    try {
+        const response = await fetch("http://localhost:3003/stations-historic/178073");
+        const data = await response.json();
+        console.log(data);
+        let contenido = ``;
+        let tableHeaders = ``;
+        let headers = ``;
+        for (const property in data[0]) {
+            headers += `<th>${property}</th>`
+        }
+
+        tableHeaders = `
+            <tr>
+                <td></td>
+                ${headers}
+            </tr>
+        `;
+        data.forEach((element, index) => {
+            let cont = ``;
+            for (const property in element) {
+                cont += `<td>${element[property]}</td>`
+
+            }
+            contenido += `
+                <tr>
+                    <td></td>
+                    ${cont}
+                </tr>`;
+        });
+        const tableHead = document.getElementById("tableHead");
+        tableHead.innerHTML = tableHeaders;
+        const tableBody = document.getElementById("tableBody");
+        tableBody.innerHTML = contenido;
+    } catch (error) {
+        alert(error)
+    }
+}
+
+window.addEventListener('load', async () => {
+    initDataTable();
+})
